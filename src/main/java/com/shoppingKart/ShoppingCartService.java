27a72ef0e299975d6f0fc46037b92e6721a2ba2e
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-06-05 20:08:19 +0200

    Fix failing test

--- src/main/java/com/shoppingKart/ShoppingCartService.java
+++ src/main/java/com/shoppingKart/ShoppingCartService.java
@@ -3,11 +3,14 @@ package com.shoppingKart;
 import com.shoppingKart.Product;
 
 public class ShoppingCartService {
+    private final ProductRepository productRepository;
+
     public ShoppingCartService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
-    public void addItem(Product product) {
-        throw new UnsupportedOperationException();
+    public void addItem(Product productFilter) {
+        this.productRepository.findProduct(productFilter);
     }
 
     public String printShoppingCart() {
